d2a8a9279747fb6b84cf38e804e58d3513fa4b3a
beatleg <seoga83@gmail.com>
2022-07-02 20:13:14 +0900

    주석 추가

--- web_application/flask_app/views/main_views.py
+++ web_application/flask_app/views/main_views.py
@@ -13,14 +13,14 @@ def index():
 @main_bp.route('/result', methods=['GET', 'POST'])
 def result():
     #request로 사용자 입력값 전달
-    jobgroup = request.form['jobgroup']
-    if request.form['career'] == '신입':
+    jobgroup = request.form['jobgroup'] # 직군 사용자 입력값
+    if request.form['career'] == '신입': # 경력여부 사용자 입력값 신입
         career = '0'
     else:
-        career = request.form['career'][3]
-    skill = ' '.join(request.form.getlist('skill'))
-    skill2 = ', '.join(request.form.getlist('skill'))
-    task = ' '.join(request.form.get('task').split())
+        career = request.form['career'][3] # 경력여부 사용자 입력값 경력
+    skill = ' '.join(request.form.getlist('skill')) # 기술스택 사용자 입력값(tfidf)
+    skill2 = ', '.join(request.form.getlist('skill')) # 기술스택 사용자 입력값(추천 키워드)
+    task = ' '.join(request.form.get('task').split()) # 업무경험 사용자 입력값(tfidf)
 
     #tfidf 해석 위한 함수 호출
     recommended_list = tfidf(jobgroup, career, skill + ' ' + task)
@@ -43,35 +43,35 @@ def tfidf(jobgroup, career, skill):
     cur = conn.cursor()
 
     #db에서 조건에 해당하는 값 불러오기
-    cur.execute(f"select * from job where category_tags like '%{jobgroup}%' and (career like '%{career}%' or career = '-1');")
+    cur.execute(f"select * from job where category_tags like '%{jobgroup}%' and (career like '%{career}%' or career = '-1');") # 사용자 입력 직군과 경력 조건
     rows = cur.fetchall()
 
     #tfidf 모델링
-    df_db = pd.DataFrame(rows, columns=["id","skill","position","requirement","task","prefer","company","logo","url","career"])
-    df_db['tfidf'] = df_db['skill'] + ' ' + df_db['requirement'] + ' ' + df_db['task'] + ' ' + df_db['prefer']
+    df_db = pd.DataFrame(rows, columns=["id","skill","position","requirement","task","prefer","company","logo","url","career"]) # DB 데이터 -> 데이터프레임
+    df_db['tfidf'] = df_db['skill'] + ' ' + df_db['requirement'] + ' ' + df_db['task'] + ' ' + df_db['prefer'] # 기술스택, 디테일 합치기
 
-    df_tfidf = pd.Series(df_db['tfidf'])
-    df_tfidf.loc[len(df_tfidf)] = skill
+    df_tfidf = pd.Series(df_db['tfidf']) # tfidf 위한 시리즈 생성
+    df_tfidf.loc[len(df_tfidf)] = skill # 시리즈에 사용자 입력값 추가
 
-    tfidf_vect = TfidfVectorizer(max_features=100)
+    tfidf_vect = TfidfVectorizer(max_features=100) # tfidf 객체 생성
 
-    dtm_tfidf = tfidf_vect.fit_transform(df_tfidf)
+    dtm_tfidf = tfidf_vect.fit_transform(df_tfidf) # tfidf fit 수행
 
-    dtm_tfidf = pd.DataFrame(dtm_tfidf.todense(), columns=tfidf_vect.get_feature_names())
+    dtm_tfidf = pd.DataFrame(dtm_tfidf.todense(), columns=tfidf_vect.get_feature_names()) # dtm 테이블 생성(문서-단어 행렬)
 
     #유사도 분석
-    nn = NearestNeighbors(n_neighbors=min(11, len(df_tfidf)), algorithm='kd_tree')
-    nn.fit(dtm_tfidf)
+    nn = NearestNeighbors(n_neighbors=min(11, len(df_tfidf)), algorithm='kd_tree') # NearestNeighbors 객체 생성
+    nn.fit(dtm_tfidf) # NearestNeighbors fit 수행
 
-    idx = nn.kneighbors([dtm_tfidf.iloc[len(dtm_tfidf)-1]])
+    idx = nn.kneighbors([dtm_tfidf.iloc[len(dtm_tfidf)-1]]) # 유사도 분석 결과 (유사도, index)
 
-    recommended = list()
+    recommended = list() # 추천 결과 list에 저장
     flag = True
     for factor, i in zip(idx[0][0], idx[1][0]):
         if flag:
             flag = False
             continue
-        recommended.append([df_db.loc[i,'url'],df_db.loc[i,'logo'],df_db.loc[i,'company'],df_db.loc[i,'position']])
+        recommended.append([df_db.loc[i,'url'],df_db.loc[i,'logo'],df_db.loc[i,'company'],df_db.loc[i,'position']]) # [url, 로고 url, 회사이름, 잡포지션]
 
     cur.close()
     conn.close()
